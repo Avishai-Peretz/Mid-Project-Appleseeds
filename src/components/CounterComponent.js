452407ce5e7a59e8e4a6f83e886ac085d5018385
Avishai-Peretz <Avishai651@gmail.com>
2022-06-09 15:16:26 +0300

    Countdown fix v2

--- src/components/CounterComponent.js
+++ src/components/CounterComponent.js
@@ -5,21 +5,21 @@ import React, { useEffect, useState } from 'react';
 
 export function CounterComponent() {
 
-    const countDownTime = new Date(2022, 8, 12, 16,30).getTime();
+    const countDownTime = new Date(2022, 8, 12, 16,30,).getTime() ;
     const now = new Date().getTime();
     const diff = countDownTime - now;
     let [count, setCount] = useState({
-        days: Math.floor(diff / (1000 * 60 * 60 * 24)),
+        days: Math.floor(diff / (1000 * 60 * 60 * 24)) - 30,
         hours: Math.floor(diff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)),
         minutes: Math.floor(diff % (1000 * 60 * 60) / (1000 * 60)),
         seconds: Math.floor(diff % (1000 * 60) / 1000)
     });
     const countDownFunction = () => {    
-            const countDownTime = new Date(2022, 8, 12, 16,30).getTime();
+            const countDownTime = new Date(2022, 8, 12, 0, 0,).getTime() ;
             const now = new Date().getTime();
             const diff = countDownTime - now;
             setCount({
-                days: Math.floor(diff / (1000 * 60 * 60 * 24)),
+                days: Math.floor(diff / (1000 * 60 * 60 * 24)) - 30,
                 hours: Math.floor(diff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)),
                 minutes: Math.floor(diff % (1000 * 60 * 60) / (1000 * 60)),
                 seconds: Math.floor(diff % (1000 * 60) / 1000)
